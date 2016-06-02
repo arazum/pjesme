a18f64078d33ef07ce6f973f5550a903c22e008d
Antun Razum <antun.razum@gmail.com>
2016-06-02 11:56:38 +0200

    default filename

--- pjesme.py
+++ pjesme.py
@@ -8,10 +8,8 @@ import time
 import os
 import sys
 
-if len(sys.argv) < 1:
-    files = ['popis.txt']
-else:
-    files = sys.argv[1:]
+
+LIST_FILE = 'list.txt'
 
 COOKIEJAR = 'tmp/{}.cookie'
 OUTPUT = 'songs/{}.mp3'
@@ -24,12 +22,17 @@ YOUTUBE_URL = 'http://www.youtube.com{}'
 CONVERT_URL = 'http://www.flv2mp3.org/convert/'
 DOWNLOAD_URL = 'http://www.flv2mp3.org/download/direct/mp3/yt_{}/'
 
+
+if len(sys.argv) <= 1:
+    files = [LIST_FILE]
+else:
+    files = sys.argv[1:]
+
 names = []
 
 for filename in files:
     f = open(filename, 'r')
     names.extend(map(lambda x: x[:-1], f.readlines()))
-
 
 print 'Requesting conversion...'
 
